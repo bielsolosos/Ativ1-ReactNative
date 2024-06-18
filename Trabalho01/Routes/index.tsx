0663270ede1187ade7b37e2c83ad16afeb7f9907
bielsolosos <bielrochasantoscoutinho@gmail.com>
2024-06-18 17:57:22 -0300

    estilizando botões

--- Trabalho01/Routes/index.tsx
+++ Trabalho01/Routes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import Feather from 'react-native-vector-icons/Feather'
 import Home from '../Screens/Home';
 import Sobre from '../Screens/Sobre';
 import Ajuda from '../Screens/Ajuda';
@@ -12,10 +13,28 @@ export default function Routes() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home}/>
-        <Tab.Screen name='Sobre' component={Sobre}/>
-        <Tab.Screen name='Ajuda' component={Ajuda}/>
-        <Tab.Screen name='Contato' component={Contato}/>
+        <Tab.Screen name='Home' component={Home}  options={{
+            tabBarInactiveTintColor:'black',
+            tabBarIcon: ({ color, size }) => {
+              return <Feather name="home" color={color} size={size} />;
+            }}} />
+
+  
+        <Tab.Screen name='Sobre' component={Sobre} options={{
+            tabBarInactiveTintColor:'black',
+            tabBarIcon: ({ color, size }) => {
+              return <Feather name="info" color={color} size={size} />;
+            }}} />
+        <Tab.Screen name='Ajuda' component={Ajuda}options={{
+            tabBarInactiveTintColor:'black',
+            tabBarIcon: ({ color, size }) => {
+              return <Feather name="help-circle" color={color} size={size} />;
+            }}} />
+        <Tab.Screen name='Contato' component={Contato}options={{
+            tabBarInactiveTintColor:'black',
+            tabBarIcon: ({ color, size }) => {
+              return <Feather name="phone" color={color} size={size} />;
+            }}} />
       </Tab.Navigator>
     </NavigationContainer>
   );
